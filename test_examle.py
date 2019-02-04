bf4b39f85ccfe7e9ed9b8d826ffbc95ba2dde03f
Pierre <pstoiko@gmail.com>
2019-02-04 17:31:20 -0500

    Test failed for Google search and passed for Facebook

---
+++ test_examle.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://google.com/")
+    driver.find_element_by_name("q").send_keys('webdriver')
+    driver.find_element_by_name('btnK').click()
+    WebDriverWait(driver, 10).until(EC.title_is('webdriver - Google Search'))
+
+
+def test_example_b(driver):
+    driver.get("http://facebook.com/")
+    WebDriverWait(driver, 10).until(EC.title_is('Facebook - Log In or Sign Up'))

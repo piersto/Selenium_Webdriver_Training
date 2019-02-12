6704734081603565830293c44ee2c4a0d7e26a4c
PStoiko <pstoiko@gmail.com>
2019-02-12 16:59:21 -0500

    Verification of the Template header is done with Wait

--- test_task_7.py
+++ test_task_7.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
-    wait = WebDriverWait(driver, 10)  # seconds
+
     request.addfinalizer(wd.quit)
     return wd
 
@@ -21,6 +21,7 @@ def test_login(driver):
     driver.find_element_by_name("login").click()
     WebDriverWait(driver, 10).until(EC.title_is('My Store'))
     driver.find_element_by_css_selector("li a[href$='appearance&doc=template']").click()
-    driver.wait.until(EC.presence_of_element_located((By.XPATH, "//h1[contains(., 'Template')]")))
+    wait = WebDriverWait(driver, 10)  # seconds
+    wait.until(EC.presence_of_element_located((By.XPATH, "//h1[contains(., 'Template')]")))
     #driver.find_element_by_xpath("//h1[contains(., 'Template')]")
 

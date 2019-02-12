ad2d8158f681e43dd9758e5173d56d6b7eae6d6e
PStoiko <pstoiko@gmail.com>
2019-02-12 16:57:18 -0500

    Started to change verification of the Template header for use of Waite

--- test_task_7.py
+++ test_task_7.py
@@ -2,11 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
+    wait = WebDriverWait(driver, 10)  # seconds
     request.addfinalizer(wd.quit)
     return wd
 
@@ -18,5 +21,6 @@ def test_login(driver):
     driver.find_element_by_name("login").click()
     WebDriverWait(driver, 10).until(EC.title_is('My Store'))
     driver.find_element_by_css_selector("li a[href$='appearance&doc=template']").click()
-    driver.find_element_by_xpath("//h1[contains(., 'Template')]")
+    driver.wait.until(EC.presence_of_element_located((By.XPATH, "//h1[contains(., 'Template')]")))
+    #driver.find_element_by_xpath("//h1[contains(., 'Template')]")
 

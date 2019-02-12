12c735c94e998c3806183560e6082bf0895ff8fb
PStoiko <pstoiko@gmail.com>
2019-02-12 16:40:13 -0500

    Added click on Appearance button

--- test_task_7.py
+++ test_task_7.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Ie()
+    wd = webdriver.Chrome()
     request.addfinalizer(wd.quit)
     return wd
 
@@ -17,3 +17,5 @@ def test_login(driver):
     driver.find_element_by_name("password").send_keys('admin')
     driver.find_element_by_name("login").click()
     WebDriverWait(driver, 10).until(EC.title_is('My Store'))
+    driver.find_element_by_css_selector("li a[href$='appearance&doc=template']").click()
+

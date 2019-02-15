ddd4a136272b844f92f986811778b0e95753a722
PStoiko <pstoiko@gmail.com>
2019-02-15 09:33:23 -0500

    Task #8, test_sticker_presence minor changes in locators

--- test_task_8.py
+++ test_task_8.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 @pytest.fixture
 def driver(request):
     wd = webdriver.Chrome()
@@ -10,11 +11,12 @@ def driver(request):
     return wd
 
 
-def test_stiker_presense(driver):
+def test_sticker_presence(driver):
     driver.get("http://localhost/litecart/en/")
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
-    products = driver.find_elements_by_css_selector('div.image-wrapper')
+
+    products = driver.find_elements_by_css_selector('.image-wrapper')
     for sticker in products:
-        sticker = driver.find_elements_by_css_selector('div.sticker')
+        sticker = driver.find_elements_by_css_selector('.sticker')
         assert len(products) == len(sticker)
         print(sticker)

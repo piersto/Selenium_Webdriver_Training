2eafcda18959a8244c5bf14c4ec1b76e067a7468
PStoiko <pstoiko@gmail.com>
2019-02-18 15:36:02 -0500

    Started test_zones_are_sorted

--- test_task_9.py
+++ test_task_9.py
@@ -12,6 +12,29 @@ def driver(request):
     return wd
 
 
+def test_areas_are_sorted(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name("username").send_keys('admin')
+    driver.find_element_by_name("password").send_keys('admin')
+    driver.find_element_by_name("login").click()
+    WebDriverWait(driver, 10).until(EC.title_is('My Store'))
+
+    driver.find_element_by_css_selector("li a[href$='countries&doc=countries']").click()
+    wait = WebDriverWait(driver, 10)  # seconds
+    wait.until(EC.presence_of_element_located((By.XPATH, "//h1[contains(., 'Countries')]")))
+
+    countries_list = []
+    table = driver.find_element_by_css_selector('tbody')
+    rows = table.find_elements_by_css_selector('tr.row')
+    for row in rows:
+        cell = row.find_elements_by_css_selector('td')
+        name = cell[4].text
+        countries_list.append(name)
+    assert countries_list == sorted(countries_list)
+    # find cells[5] with ... !=0
+    # click cell[4]
+
+
 def test_countries_are_sorted(driver):
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys('admin')
@@ -31,3 +54,6 @@ def test_countries_are_sorted(driver):
         name = cell[4].text
         countries_list.append(name)
     assert countries_list == sorted(countries_list)
+    # find cells[5] with ... !=0
+    # click cell[4]
+
---
+++ test_task_9_2.py
@@ -0,0 +1,41 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_zones_are_sorted(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name("username").send_keys('admin')
+    driver.find_element_by_name("password").send_keys('admin')
+    driver.find_element_by_name("login").click()
+    WebDriverWait(driver, 10).until(EC.title_is('My Store'))
+
+    driver.find_element_by_css_selector("li a[href$='geo_zones&doc=geo_zones']").click()
+    wait = WebDriverWait(driver, 10)  # seconds
+    wait.until(EC.presence_of_element_located((By.XPATH, "//h1[contains(., 'Geo Zones')]")))
+
+    elements = driver.find_elements_by_xpath("tr[contains(@class, 'row')]")
+    for element in range(len(elements)):
+        elements[element].click()
+        wait = WebDriverWait(driver, 10)  # seconds
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "h1")))
+        time.sleep(2)
+
+        states_list = []
+        table = driver.find_element_by_css_selector('#table-zones')
+        rows = table.find_elements_by_name('zones')
+        for row in rows:
+            cell = row.find_element_by_name('zones')
+            name = cell[2].get_attribute('innerText')
+            states_list.append(name)
+        assert states_list == sorted(states_list)
+

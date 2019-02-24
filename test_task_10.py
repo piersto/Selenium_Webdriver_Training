bad7100a26cb5e10b1f3984c246e3701b73c27b5
Pierre <pstoiko@gmail.com>
2019-02-24 13:31:02 -0500

    test_task_10 added price verification. Test passed. Have to modify text to get_attribute.

--- test_task_10.py
+++ test_task_10.py
@@ -2,6 +2,8 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+
 
 
 @pytest.fixture
@@ -16,9 +18,34 @@ def test_first_item(driver):
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
 
     name_on_home_page = driver.find_element_by_css_selector('div#box-most-popular div.name').text
+    price = driver.find_element_by_css_selector()
     driver.find_element_by_css_selector('.product').click()
     name_on_product_page = driver.find_element_by_css_selector('div#box-product h1').text
     assert name_on_home_page == name_on_product_page
+
+
+def test_first_price(driver):
+    driver.get("http://localhost/litecart/en/")
+    WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
+
+    try:
+        price_on_home_page = driver.find_element_by_css_selector('div.price-wrapper span.price').text
+    except NoSuchElementException:
+        pass  # не найден ну и ладно
+
+    campaign_price_on_home_page = driver.find_element_by_css_selector('div.price-wrapper strong.campaign-price').text
+
+    driver.find_element_by_css_selector('.product').click()
+
+    try:
+        price_on_product_page = driver.find_element_by_css_selector('div.price-wrapper span.price').text
+    except NoSuchElementException:
+        pass  # не найден ну и ладно
+
+    campaign_price_on_product_page = driver.find_element_by_css_selector('div.price-wrapper strong.campaign-price').text
+    assert price_on_home_page == price_on_product_page
+    assert campaign_price_on_home_page == campaign_price_on_product_page
+
 
 
 

3897a79dbebe685b7e3dbc7b0894690bf18f4a3c
PStoiko <pstoiko@gmail.com>
2019-02-25 16:23:58 -0500

    To test_campaign_price_color added hex

--- test_task_10.py
+++ test_task_10.py
@@ -4,11 +4,13 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import time
+from selenium.webdriver.support.color import Color
+
 
 
 @pytest.fixture
 def driver(request):
-    wd = webdriver.Chrome()
+    wd = webdriver.Firefox()
     request.addfinalizer(wd.quit)
     return wd
 
@@ -45,7 +47,24 @@ def test_regular_price(driver):
     assert price_on_home_page == price_on_product_page
 
 
-'''def test_price_color(driver):
+def test_campaign_price_color(driver):
+    driver.get("http://localhost/litecart/en/")
+    WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
+
+    price_color_on_home_page = driver.find_element_by_css_selector('div#box-campaigns strong.campaign-price').value_of_css_property("color")
+    print(Color.from_string(price_color_on_home_page).hex)
+    print(price_color_on_home_page)
+
+    '''blue_color_from_price = Color.from_string('blue').rgba
+    green_color_from_price = Color.from_string('green').rgba
+    blue = 0
+    green = 0
+    print(blue_color_from_price)
+    assert blue_color_from_price == blue
+    assert green_color_from_price == green'''''
+
+
+def test_price_color(driver):
     driver.get("http://localhost/litecart/en/")
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
 
@@ -61,22 +80,28 @@ def test_regular_price(driver):
     print(campaign_price_color_on_home_page)
     time.sleep(3)
 
-    assert campaign_price_color_on_home_page[1:3] == campaign_price_color'''
+    assert campaign_price_color_on_home_page[1:3] == campaign_price_color
 
 
-def test_campaign_price_color(driver):
+'''def test_campaign_price_color_martin(driver):
     driver.get("http://localhost/litecart/en/")
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
 
     campaign_price_color_green = ' 0'
     campaign_price_color_blue = ' 0'
-    campaign_price_color_on_home_page = driver.find_element_by_css_selector('div.price-wrapper strong.campaign-price').value_of_css_property("color")
+    campaign_price_color_on_home_page = driver.find_element_by_css_selector('div#box-campaigns strong.campaign-price').value_of_css_property("color")
     print(campaign_price_color_on_home_page)
     array_campaign_price_color_on_home_page = campaign_price_color_on_home_page.split(',')
     assert array_campaign_price_color_on_home_page[1] == campaign_price_color_green
-    assert array_campaign_price_color_on_home_page[2] == campaign_price_color_blue
+    assert array_campaign_price_color_on_home_page[2] == campaign_price_color_blue'''
 
 
-def test_removed_price_color(driver):
+'''def test_removed_price_color(driver):
     driver.get("http://localhost/litecart/en/")
-    WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
+    WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))'''
+
+
+
+print(Color.from_string('#00ff33').rgba)
+print(Color.from_string('rgb(1, 255, 3)').hex)
+print(Color.from_string('blue').rgba)

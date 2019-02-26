095efb723b4f63c18137b4402f9034bbc1f523a7
Pierre <pstoiko@gmail.com>
2019-02-25 20:38:30 -0500

    Added test_campaign_price_color_on_product_page and test_price_color_on_product_page

--- test_task_10.py
+++ test_task_10.py
@@ -2,10 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-import time
 from selenium.webdriver.support.color import Color
-
 
 
 @pytest.fixture
@@ -47,7 +44,7 @@ def test_regular_price(driver):
     assert price_on_home_page == price_on_product_page
 
 
-def test_campaign_price_color(driver):
+def test_campaign_price_color_on_home_page(driver):
     driver.get("http://localhost/litecart/en/")
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
 
@@ -60,44 +57,56 @@ def test_campaign_price_color(driver):
     assert blue_color_on_product_page == blue
 
 
-def test_price_color(driver):
+def test_price_color_on_home_page(driver):
     driver.get("http://localhost/litecart/en/")
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
 
-    try:
-        price_color_on_home_page = driver.find_element_by_css_selector('div.price-wrapper span.price').value_of_css_property("color")
-        price_color = (0, 0)
-        assert price_color_on_home_page[0:-1] == price_color
-    except NoSuchElementException:
-        pass  # не найден ну и ладно
-    price_color = (0, 0)
-    campaign_price_color = (0, 0)
-    campaign_price_color_on_home_page = driver.find_element_by_css_selector('div.price-wrapper strong.campaign-price').value_of_css_property("color")
-    print(campaign_price_color_on_home_page)
-    time.sleep(3)
-
-    assert campaign_price_color_on_home_page[1:3] == campaign_price_color
+    get_color = driver.find_element_by_css_selector('div#box-campaigns s.regular-price').value_of_css_property("color")
+    red_color_on_product_page = Color.from_string(get_color).hex[1:3]
+    green_color_on_product_page = Color.from_string(get_color).hex[3:5]
+    blue_color_on_product_page = Color.from_string(get_color).hex[5:7]
+    assert green_color_on_product_page == red_color_on_product_page
+    assert blue_color_on_product_page == red_color_on_product_page
+    assert green_color_on_product_page == blue_color_on_product_page
 
 
-'''def test_campaign_price_color_martin(driver):
+def test_campaign_price_color_on_product_page(driver):
     driver.get("http://localhost/litecart/en/")
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
 
-    campaign_price_color_green = ' 0'
-    campaign_price_color_blue = ' 0'
-    campaign_price_color_on_home_page = driver.find_element_by_css_selector('div#box-campaigns strong.campaign-price').value_of_css_property("color")
-    print(campaign_price_color_on_home_page)
-    array_campaign_price_color_on_home_page = campaign_price_color_on_home_page.split(',')
-    assert array_campaign_price_color_on_home_page[1] == campaign_price_color_green
-    assert array_campaign_price_color_on_home_page[2] == campaign_price_color_blue'''
+    driver.find_element_by_css_selector('div#box-campaigns .product').click()
+
+    get_color = driver.find_element_by_css_selector('div.price-wrapper strong.campaign-price').value_of_css_property("color")
+    green_color_on_product_page = Color.from_string(get_color).hex[3:5]
+    blue_color_on_product_page = Color.from_string(get_color).hex[5:7]
+    green = '00'
+    blue = '00'
+    assert green_color_on_product_page == green
+    assert blue_color_on_product_page == blue
 
 
-'''def test_removed_price_color(driver):
+def test_price_color_on_product_page(driver):
     driver.get("http://localhost/litecart/en/")
-    WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))'''
+    WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
+
+    driver.find_element_by_css_selector('div#box-campaigns .product').click()
+
+    get_color = driver.find_element_by_css_selector('div.price-wrapper s.regular-price').value_of_css_property("color")
+    red_color_on_product_page = Color.from_string(get_color).hex[1:3]
+    green_color_on_product_page = Color.from_string(get_color).hex[3:5]
+    blue_color_on_product_page = Color.from_string(get_color).hex[5:7]
+    assert green_color_on_product_page == red_color_on_product_page
+    assert blue_color_on_product_page == red_color_on_product_page
+    assert green_color_on_product_page == blue_color_on_product_page
 
 
 
-print(Color.from_string('#00ff33').rgba)
-print(Color.from_string('rgb(1, 255, 3)').hex)
-print(Color.from_string('blue').rgba)
+
+
+
+
+
+
+
+
+

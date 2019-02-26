87f1fe31ca63524e1f5e80df817f226b31091c88
PStoiko <pstoiko@gmail.com>
2019-02-26 16:23:03 -0500

    Started task 12 +1

--- test_task_12.py
+++ test_task_12.py
@@ -20,7 +20,6 @@ def test_item(driver):
     driver.find_element_by_name("login").click()
     WebDriverWait(driver, 10).until(EC.title_is('My Store'))
 
-    driver.find_element_by_id("doc-catalog").click()
+    driver.find_element_by_css_selector("li a[href$='/?app=catalog&doc=catalog']").click()
     wait = WebDriverWait(driver, 10)  # seconds
     wait.until(EC.presence_of_element_located((By.XPATH, "//h1[contains(., 'Catalog')]")))
-

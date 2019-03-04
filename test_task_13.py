72b70c1aa0c29be4fd929460fd5a80f0e530fb00
PStoiko <pstoiko@gmail.com>
2019-03-04 16:56:50 -0500

    Started task 13 passed. +1

--- test_task_13.py
+++ test_task_13.py
@@ -29,14 +29,10 @@ def test_add_product(driver):
     add_element_to_the_basket(driver)
     wait = WebDriverWait(driver, 10)  # seconds
     wait.until(lambda d: d.find_element_by_css_selector('span.quantity').text != quantity_start)
-    # Go back to Home page
-    driver.find_element_by_css_selector("li a[href$='/litecart/en/']").click()
 
     add_element_to_the_basket(driver)
     wait = WebDriverWait(driver, 10)  # seconds
     wait.until(lambda d: d.find_element_by_css_selector('span.quantity').text != quantity_start)
-    # Go back to Home page
-    driver.find_element_by_css_selector("li a[href$='/litecart/en/']").click()
 
     add_element_to_the_basket(driver)
     wait = WebDriverWait(driver, 10)  # seconds
@@ -64,19 +60,12 @@ def add_element_to_the_basket(driver):
     if len(driver.find_elements_by_css_selector("[name='options[Size]']")) > 0:
         # If element is present, select 'Small' from drop-down menu
         Select(driver.find_element_by_name("options[Size]")).select_by_visible_text("Small")
+        driver.find_element_by_name('add_cart_product').click()
     else:
         # If not present Click on 'Add to cart' button
         driver.find_element_by_name('add_cart_product').click()
         time.sleep(2)
 
-    # WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element_value('0'))
-
     # Go back to Home page
     driver.find_element_by_css_selector("li a[href$='/litecart/en/']").click()
-
-
-def verify_text_is_changed(driver):
-    quantity_start = driver.find_element_by_css_selector('span.quantity').text
-    quantity_end = driver.find_element_by_css_selector('span.quantity').text
-    assert quantity_end != quantity_start
 

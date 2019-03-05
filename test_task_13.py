c4c2213c3d75bcde06e0ac3e2c3902f0f4c1161d
Pierre <pstoiko@gmail.com>
2019-03-04 20:44:57 -0500

    test_task_13 passed with time. Moved 'Wait' to help function

--- test_task_13.py
+++ test_task_13.py
@@ -24,27 +24,22 @@ def test_add_product(driver):
     driver.get("http://localhost/litecart/en/")
     WebDriverWait(driver, 10).until(EC.title_is('Online Store | My Store'))
 
-    quantity_start = driver.find_element_by_css_selector('span.quantity').text
+    add_element_to_the_basket(driver)
 
     add_element_to_the_basket(driver)
-    wait = WebDriverWait(driver, 10)  # seconds
-    wait.until(lambda d: d.find_element_by_css_selector('span.quantity').text != quantity_start)
 
     add_element_to_the_basket(driver)
-    wait = WebDriverWait(driver, 10)  # seconds
-    wait.until(lambda d: d.find_element_by_css_selector('span.quantity').text != quantity_start)
-
-    add_element_to_the_basket(driver)
-    wait = WebDriverWait(driver, 10)  # seconds
-    wait.until(lambda d: d.find_element_by_css_selector('span.quantity').text != quantity_start)
 
     # Go to the basket
     driver.find_element_by_css_selector("a[href$='/en/checkout']").click()
 
-    for buttons in driver.find_elements_by_css_selector("[name='remove_cart_item']"):
-        button_remove = wait.until(EC.visibility_of_element_located((By.NAME, "remove_cart_item")))
-        button_remove.click()
-        driver.find_elements_by_css_selector("[name='remove_cart_item']")
+    buttons = driver.find_elements_by_css_selector("[name='remove_cart_item']")
+    for button in buttons:
+        if len(driver.find_elements_by_css_selector("td.item")) > 0:
+            wait = WebDriverWait(driver, 10)  # seconds
+            button = wait.until(EC.visibility_of_element_located((By.NAME, "remove_cart_item")))
+            button.click()
+            driver.find_elements_by_css_selector("[name='remove_cart_item']")
 
     # Go back to Home page
     driver.find_element_by_css_selector("li a[href$='/litecart/en/']").click()
@@ -52,10 +47,9 @@ def test_add_product(driver):
 
 
 def add_element_to_the_basket(driver):
-    driver.get("http://localhost/litecart/en/")
-
     driver.find_element_by_css_selector('div#box-most-popular .product').click()
 
+    quantity_start = driver.find_element_by_css_selector('span.quantity').text
     # Find out if element present
     if len(driver.find_elements_by_css_selector("[name='options[Size]']")) > 0:
         # If element is present, select 'Small' from drop-down menu
@@ -65,6 +59,8 @@ def add_element_to_the_basket(driver):
         # If not present Click on 'Add to cart' button
         driver.find_element_by_name('add_cart_product').click()
         time.sleep(2)
+    wait = WebDriverWait(driver, 10)  # seconds
+    wait.until(lambda d: d.find_element_by_css_selector('span.quantity').text != quantity_start)
 
     # Go back to Home page
     driver.find_element_by_css_selector("li a[href$='/litecart/en/']").click()

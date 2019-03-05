6324058785d7e55824c816d33c75aed7cb296fbb
Pierre <pstoiko@gmail.com>
2019-03-04 21:59:03 -0500

    test_task_13 passed with time. Moved 'Wait' to help function +1

--- test_task_13.py
+++ test_task_13.py
@@ -1,12 +1,6 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-import os.path
-import random
-import string
 import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC

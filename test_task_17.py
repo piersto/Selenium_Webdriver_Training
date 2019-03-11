4a3641f754b3c7c3672b25b57b7b3c6c0a48c0e1
Pierre <pstoiko@gmail.com>
2019-03-10 23:27:09 -0400

    Start  test_task_17 + clicks on all links

--- test_task_17.py
+++ test_task_17.py
@@ -21,4 +21,9 @@ def test_links_are_open_in_new_window(driver):
 
     driver.get('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1')
     time.sleep(3)
-    driver.find_element_by_css_selector("tr > td:nth-child(5) a[href*='product_id=']").click()
+    product_links = driver.find_elements_by_css_selector("tr > td:nth-child(5) a[href*='product_id=']")
+    for i in range(len(product_links)):
+        product_links[i].click()
+        driver.get('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1')
+        product_links = driver.find_elements_by_css_selector("tr > td:nth-child(5) a[href*='product_id=']")
+
